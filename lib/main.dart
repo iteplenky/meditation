39a18d3d62cfac953e28fdedba3d737797be1926
Sergey-bot-sys <79085670+Sergey-bot-sys@users.noreply.github.com>
2021-10-25 16:15:56 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:untitled2/pages/main_windowsw.dart';
+import 'package:untitled2/pages/home.dart';
+import 'package:untitled2/pages/snatie_stressa.dart';
+import 'package:untitled2/pages/son.dart';
+import 'package:untitled2/pages/time_out.dart';
 
 void main() => runApp(MaterialApp(
-  home: Main(),
-));
-class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Главная'),
-        backgroundColor: Colors.white24,
-      ),
-      body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Icon(Icons.water_damage, size: 60, ),
-                TextButton(onPressed: () {},
-                  child: Text('Главная', style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.black54,
-                  ),),
-                )
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Icon(Icons.music_note_outlined, size: 60,),
-                TextButton(onPressed: () {},
-                  child: Text('Звуки', style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.black54,
-                  ),),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+  initialRoute: '/',
+  routes: {
+    '/': (contex) => mainwindows(),
+    '/s': (context) => athome(),
+    '/son': (context) => sonlist(),
+    '/stres': (context) => stress(),
+    '/timeout': (context) => time_out(),
+  },
+));
+
