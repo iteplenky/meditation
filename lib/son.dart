640c7a2405ac7eec8e3456360e5da579c20866f2
Sergey-bot-sys <79085670+Sergey-bot-sys@users.noreply.github.com>
2021-11-02 12:37:49 +0300

    Update son.dart

--- lib/son.dart
+++ lib/son.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class sonlist extends StatelessWidget {
@@ -11,12 +14,78 @@ class sonlist extends StatelessWidget {
         title: Text('Сон'),
     backgroundColor: Colors.white24,
     ),
-    body:
-        Column(
-          children: [
-            Text('Список звуков сна'),
-          ],
-        ),
+    body:SafeArea(
+      child:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton.icon(label: Text('Сон 1',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+                onPressed: () {
+                  final play=AudioCache();
+                  play.play('locomotive_steam_train_whistle.mp3');
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white38),
+                ),
+                icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton.icon(label: Text('Сон 2',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+                onPressed: () {
+                  final play=AudioCache();
+                  play.play('locomotive_steam_train_whistle.mp3');
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white38),
+                ),
+                icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  OutlinedButton.icon(label: Text('Сон 3',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                    onPressed: () {
+                      final play=AudioCache();
+                      play.play('locomotive_steam_train_whistle.mp3');
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white38),
+                    ),
+                    icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
     );
   }
 }
