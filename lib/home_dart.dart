8114ff40db41f040368ea1515b50e8372567449d
Sergey-bot-sys <79085670+Sergey-bot-sys@users.noreply.github.com>
2021-11-02 12:36:18 +0300

    Update home_dart.dart

--- lib/home_dart.dart
+++ lib/home_dart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled2/pages/home.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class athome extends StatefulWidget {
   const athome({Key? key}) : super(key: key);
@@ -17,35 +18,104 @@ class _athomeState extends State<athome> {
         backgroundColor: Colors.white24,
       ),
       body: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Column(
-
-              mainAxisAlignment: MainAxisAlignment.end,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.water_damage, size: 60, ),
-                TextButton(onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(context, '/', (route)=>false);
-                },
-                  child: Text('Главная', style: TextStyle(
-                    fontSize: 26,
+                OutlinedButton.icon(label: Text('Звуки дождя',
+                  style: TextStyle(
+                    fontSize: 20,
                     color: Colors.black,
-                  ),),
+                  ),
+                ),
+                  onPressed: () {
+                    final play=AudioCache();
+                    play.play('locomotive_steam_train_whistle.mp3');
+
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white38),
+                  ),
+                  icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,),
                 )
               ],
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.music_note_outlined, size: 60,),
-                TextButton(onPressed: () {
-                },
-                  child: Text('Звуки', style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.black54,
-                  ),),
+                OutlinedButton.icon(label: Text('Звуки костра',
+                  style: TextStyle(fontSize: 20, color: Colors.black,),
+                ),
+                  onPressed: () {
+                    final play=AudioCache();
+                    play.play('locomotive_steam_train_whistle.mp3');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white38),
+                  ),
+                  icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,),
                 )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    OutlinedButton.icon(label: Text('Шум гор',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                        onPressed: () {
+                          final play=AudioCache();
+                          play.play('locomotive_steam_train_whistle.mp3');
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.white38),
+                        ),
+                        icon: Icon(Icons.play_arrow, size: 60, color: Colors.black,))
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.water_damage, size: 60, ),
+                    TextButton(onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(context, '/', (route)=>false);
+                    },
+                      child: Text('Главная',
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.music_note_outlined, size: 60,),
+                    TextButton(onPressed: () {},
+                      child: Text('Звуки',
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ],
@@ -54,3 +124,4 @@ class _athomeState extends State<athome> {
     );
   }
 }
+
